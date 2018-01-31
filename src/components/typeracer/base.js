860c4f747c7a00cdf58ae758d56468ea1863a95a
prince <prince.verma@daffodilsw.com>
2018-01-31 21:06:35 +0530

    implemented text input and show the text that has to be typed.

--- src/components/typeracer/base.js
+++ src/components/typeracer/base.js
@@ -2,21 +2,28 @@ import React from 'react';
 import * as Api from '../../utility/api'
 import {sampleTextURL} from '../../config/config';
 
-export default class Login extends React.Component {
+export default class Base extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      text:""
+      unTypedText: "",
+      typedText: "",
+      inputValue : "",
     }
   }
 
-  async componentWillMount(){
-    try{
-      let result  = await Api.get(sampleTextURL);
-      let text = result.text_out;
-      text = text.replace(new RegExp('<p>|</p>|\n','g'), "");
-      this.setState({text:text.toString()})
-    }catch (err){
+  onChange = (text) => {
+    let {unTypedText, typedText} = this.state;
+    console.log(text)
+  };
+
+  async componentWillMount() {
+    try {
+      let result = await Api.get(sampleTextURL);
+      let text = result.text_out ? result.text_out : "";
+      text = text.replace(new RegExp('<p>|</p>|\n', 'g'), "");
+      this.setState({unTypedText: text.toString()})
+    } catch (err) {
       alert("Error occured in getting data.");
     }
   }
--- src/components/typeracer/index.js
+++ src/components/typeracer/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import Base from './base';
 import "../css/base.css"
+import {TextField} from 'material-ui';
 
-export default class Login extends Base {
+export default class TypeRacer extends Base {
   render() {
+    let {unTypedText, typedText, inputValue} = this.state;
+
     return (
       <div className="container">
-        {this.state.text}
+        <div className={"textDiv"}>
+          <span className={"typedText"}>{typedText}</span>
+          <span className={"untypedText"}>{unTypedText}</span>
+        </div>
+        {
+          unTypedText &&
+          <TextField
+            hintText={"Type the above text here..."}
+            underlineStyle={{borderColor: "#000"}}
+            underlineFocusStyle={{borderColor: "#000"}}
+            onChange={(e) => this.onChange(e.target.value)}
+            value={inputValue}
+          />
+        }
       </div>
     )
   }
